073c037754f005776c0f80d68f992b56803657e8
Susanne <37674058+susott@users.noreply.github.com>
2025-01-27 10:41:35 +0100

    add shell script to run nginx in docker container, fix egress protocol typo

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "myapp-security-group" {
   egress {
     from_port = 0
     to_port = 0
-    protocol = "TCP"
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
@@ -110,6 +110,8 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
+  user_data = file("entry_script.sh")
+  user_data_replace_on_change = true
   tags = {
     Name: "${var.env_prefix}-server"
   }
